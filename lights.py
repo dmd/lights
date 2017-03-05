3150c8e21623ffd0f37f18eac28a4b6f852c56a5
Daniel M. Drucker <dmd@3e.org>
2017-03-05 09:48:32 -0500

    definitions

---
+++ lights.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+from gpiozero import LED, Button
+from signal import pause
+
+leds = {
+    "r0": 4,
+    "y0": 17,
+    "g0": 18,
+    "b0": 27,
+    "w0": 22,
+    "r1": 23,
+    "y1": 24,
+    "g1": 25,
+    "b1": 5,
+    "w1": 6,
+    "rgbr": 12,
+    "rgbg": 13,
+    "rgbb": 19,
+    "cf": 26,
+    "cs": 20}
+
+buttons = {
+    "r": 2,
+    "g": 3,
+    "w": 14,
+    "y": 15,
+    "ml": 10,
+    "mr": 9,
+    "sl": 23,
+    "sr": 24}
+
+
