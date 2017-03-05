19eb2cbb6f18970ad3377e9ac6ce8bc45b2c2a41
Daniel Drucker <dmd@3e.org>
2017-03-05 16:22:07 +0000

    more features

--- lights.py
+++ lights.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
+from time import sleep
+from functools import partial
 from gpiozero import LED, Button
 from signal import pause
+import sys
+
+
+# the ones commented out don't seem to be wired properly.
 
 leds = {
     "r0": 4,
@@ -21,11 +27,11 @@ leds = {
 
 buttons = {
     "r": 2,
-    "g": 3,
+#    "g": 3,
     "w": 14,
     "y": 15,
-    "ml": 10,
-    "mr": 9,
+#    "ml": 10,
+#    "mr": 9,
     "sl": 11,
     "sr": 8}
 
@@ -35,10 +41,40 @@ for (k,v) in leds.items():
 for (k,v) in buttons.items():
     buttons[k] = Button(v)
 
-    
-while True:
-    for l,v in leds.items():
+
+def ontwo(onled,blinkled):
+    leds[onled].on()
+    leds[blinkled].blink(on_time=.25,off_time=.25)
+
+def offtwo(onled,blinkled):
+    leds[onled].off()
+    leds[blinkled].off()
+
+def onoff(b, l1, l2):
+    buttons[b].when_pressed = lambda l1=l1, l2=l2: ontwo(l1, l2)
+    buttons[b].when_released = lambda l1=l1, l2=l2: offtwo(l1, l2)
+
+def allon():
+    for (k,v) in leds.items():
         v.on()
-        print l
-        raw_input()
+
+def alloff():
+    for (k,v) in leds.items():
         v.off()
+
+        
+onoff('r','r0', 'r1')
+onoff('w','cf', 'w0')
+onoff('y','y0', 'y1')
+onoff('sl','g0', 'g1')
+#onoff('sr','b0', 'b1')
+
+buttons['sr'].when_pressed = allon
+buttons['sr'].when_released = alloff
+
+
+pause()
+
+
+
+
